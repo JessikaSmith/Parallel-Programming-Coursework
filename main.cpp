a6034241e1001e382d3fdbbd82689c8ff5aac2ef
MaryKh <mariyaxod@yandex.ru>
2018-06-13 17:20:19 +0300

    Add gitignore

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <omp.h>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    omp_set_num_threads(3);
+#pragma omp parallel
+    {
+        cout << "number: " << rand() << " " << endl;
+    };
     return 0;
 }
